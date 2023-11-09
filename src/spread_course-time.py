0db5dff720de6e9f809d10db8c02b72bc2c9036c
SIQI PAN <SiQiPan2008@outlook.com>
2023-11-09 23:06:14 +0800

    Update scatterplot

--- src/spread_course-time.py
+++ src/spread_course-time.py
@@ -16,6 +16,8 @@ stat_color = {
     'Status.Dormancy': '#000000'
 }
 
+months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
+
 plt.rcParams.update({
     'font.family': 'serif',
     'font.serif': ['cmr10'],
@@ -38,14 +40,23 @@ for i in range(rows):
             colors.append(stat_color[stat[k]])
 
         subplots[i, j].scatter(arrX, arrY, s=0.5, c=colors, alpha=0.9)
-        subplots[i, j].set_yticks([-15, -10, -5, 0, 5, 10, 15])
-        subplots[i, j].set_xticks([0, 5, 10, 15])
+        subplots[i, j].set_xlim([0, 10])
+        subplots[i, j].set_ylim([-10, 10])
+        subplots[i, j].set_yticks([-10, -5, 0, 5, 10])
+        subplots[i, j].set_xticks([0, 5, 10])
+        subplots[i, j].text(9.5, -9.5, months[(cols * i + j + 4) % 12] + ' 1st', ha='right', va='bottom')
         if i != rows - 1:
             subplots[i, j].tick_params(axis='x', which='both', top=False, bottom=False, labeltop=False, labelbottom=False)
         if j != 0:
             subplots[i, j].tick_params(axis='y', which='both', left=False, right=False, labelleft=False, labelright=False)
 
-
+gr = plt.Line2D([], [], color='#33AA55', marker='o', linestyle='None', markersize=2)
+rd = plt.Line2D([], [], color='#FF0000', marker='o', linestyle='None', markersize=2)
+be = plt.Line2D([], [], color='#0000FF', marker='o', linestyle='None', markersize=2)
+bk = plt.Line2D([], [], color='#000000', marker='o', linestyle='None', markersize=2)
+blank = plt.Line2D([], [])
+fig.legend(handles=[be, gr, rd, bk], labels=['Seed', 'Developing / inter-dispersal', 'Dispersal', 'Dormancy / hold'], loc='lower center', ncol=4, bbox_to_anchor=(0.5,0.03))
+fig.text(0.926, 0.08, '(m)', ha='right', va='bottom')
 
 plt.subplots_adjust(wspace=0.1, hspace=0.05)
 plt.savefig('..\\figures\\spread_course-time.pdf', bbox_inches = 'tight')
